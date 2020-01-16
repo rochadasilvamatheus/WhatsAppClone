4bdbd47fa6ff6f21c4bc24a94124f07e78fea82f
rochamatheus <mathewsr.silva@gmail.com>
2020-01-15 23:23:27 -0300

    Initial commit

---
+++ app/src/main/java/com/android/matheusrocha/whatsapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.android.matheusrocha.whatsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void cadastrar(View view) {
+        startActivity(new Intent(this, CadastroActivity.class));
+    }
+}
